f96d557fe571eccc7247db28761a6f06bd0362cd
Jinliu Pan <panjinliu@bilibili.com>
2024-07-12 22:11:57 +0800

    领取活动库表设计

---
+++ magic-lottery-infrastructure/src/main/java/cn/pan/infrastructure/persistent/dao/IRaffleActivityAccountDayDao.java
@@ -0,0 +1,9 @@
+package cn.pan.infrastructure.persistent.dao;
+
+/**
+ * @author MrPJSix
+ * @description
+ * @date 2024/7/12 20:41
+ */
+public interface IRaffleActivityAccountDayDao {
+}
---
+++ magic-lottery-infrastructure/src/main/java/cn/pan/infrastructure/persistent/dao/IUserRaffleOrderDao.java
@@ -0,0 +1,9 @@
+package cn.pan.infrastructure.persistent.dao;
+
+/**
+ * @author MrPJSix
+ * @description 用户抽奖订单表
+ * @date 2024/7/12 20:40
+ */
+public interface IUserRaffleOrderDao {
+}
